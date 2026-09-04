09ede3d7ae1ea15b795d7a1ea6bfa6c824098d2b
Saoryvic <yasmin2i0s0t21@gmail.com>
2026-09-03 20:17:03 -0400

    Initial

---
+++ projeto7.py
@@ -0,0 +1,6 @@
+idade = int(input("digite sua idade atual: "))
+idade_para_somar = int(input("quantos anos voce que somar ? "))
+
+idade_futura = idade + idade_para_somar
+
+print(f"sua idade furuta é : {idade_futura}")
