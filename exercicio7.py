7481da22295ed24f2da0b5aabb1a6ac68faeb03f
Saoryvic <yasmin2i0s0t21@gmail.com>
2026-09-25 20:25:14 -0400

    Iitial

--- exercicio7.py
+++ exercicio7.py
@@ -1,5 +1,30 @@
-def calculo_total (preço, quantidade):
- resultado = preço * quantidade
- return resultado
-f_calculo = calculo_total (30, 15)
-print ("total", f_calculo)
+def calcular_total(preco, quantidade):
+    total = preco * quantidade
+    return total
+
+
+print("===== BIBLIOTECA ===== \n")
+print("1 - Calcular total \n")
+print("2 - Sair \n")
+
+opcao = int(input("Escolha uma opção: \n"))
+quantidade_livros = int(input("Quantidade de livros :\n "))
+
+for i in range (1, quantidade_livros +1):
+
+    livro = input(f"\nPor favor, incira o nome do livro: {quantidade_livros} \n")
+    if opcao == 1:
+        
+        preco = float(input(" Insira o preço do livro: \nR$ "))
+        quantidade = int(input("Insira a quantidade: "))
+
+        resultado = calcular_total(preco, quantidade)
+
+        print("O resutado final é: R$", resultado)
+
+    elif opcao == 2:
+        print("Programa encerrado.")
+        break
+
+else:
+     print("Usuario, sua opção está invalida ...")
